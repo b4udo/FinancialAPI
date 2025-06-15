070974670acd556b7c253c502fea8995fb97200b
Edoardo Baudino <dodobaudo16@gmail.com>
2025-06-15 18:39:56 +0200

    delete query for insert manually the first transactions

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,2 @@
 INSERT INTO financial_account (name, balance) VALUES ( 'Edoardo', 1000.0);
 INSERT INTO financial_account (name, balance) VALUES ( 'Andreia', 2000.0);
-
-INSERT INTO transaction (timestamp, amount, description, account_id) VALUES ('2025-06-15 11:00:00', 150.00, 'Pagamento bolletta', 1);
-
-INSERT INTO transaction (timestamp, amount, description, account_id) VALUES ('2025-06-15 12:30:00', 250.50, 'Bonifico ad Andrea', 1);
